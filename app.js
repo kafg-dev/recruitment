162d81a8bb11baacb571f6a5b3bb759fa0932f36
Kaye Anne Gonzales <gonzaleskayeanne@gmail.com>
2021-05-27 19:21:14 +0800

    Uupdate

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res) {
     res.sendFile(__dirname + "/index.html");
+
 });
+
+app.get("/signup", function(req,res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+function tryme() {
+    res.sendFile(__dirname + "/signup.html");
+}
 
 app.listen(3000, function() {
     console.log("Server running in port 3000");
